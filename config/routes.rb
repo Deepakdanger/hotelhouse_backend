fe2a699a439842825e3ea0aa16334a14e48b5775
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-02 19:23:10 +0530

    Add routes for show in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-  get 'houses',to: 'houses#index'
+Rails.application.routes.draw do  
+  resources :houses, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
   get 'authenticate',to: 'application#current_user'
