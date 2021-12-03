a788ba2b75b44c126fbfc793a354d9896002a0ee
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-03 12:40:46 +0530

    Change show fuction of house controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,6 +6,12 @@ class HousesController < ApplicationController
 
   def show
     @house = House.find(params[:id])
-    render json: @house
+    if(Favourite.find_by(house_id: @house.id, user_id: current_user.id))
+      @fav=true
+    else
+      @fav=false
+    end
+
+    render json: { data: @house, status: @fav }
   end
 end
