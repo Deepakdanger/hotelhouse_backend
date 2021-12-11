8e1acef59624b7b68f2390cda18fd899481842a1
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-11 15:19:01 +0530

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'deep' , email: 'deep@mail.com' , password: '123456' , password_confirmation: '123456')
+User.create!(name: 'far' , email: 'far@gmail.com' , password: '123456' , password_confirmation: '123456')
+
+
+h1=House.create(title: "Big rom furnished",cost: "50000",location: "U.S",category:"Room")
+h2=House.create(title: "Small room",cost: "40100",location: "India",category:"Room")
+h3=House.create(title: "Small Semi room",cost: "20100",location: "U.S",category:"Room")
+h4=House.create(title: "Medium Size room",cost: "35670",location: "Japan",category:"Room")
+h5=House.create(title: "Big room Unfurnished",cost: "31000",location: "U.S",category:"Room")
+
+h4=House.create(title: "Small wood house",cost: "50000",location: "India",category:"House")
+h4=House.create(title: "Furnished house",cost: "43000",location: "Japan",category:"House")
+h4=House.create(title: "Beach House",cost: "34000",location: "U.S",category:"House")
+h4=House.create(title: "Villa for Rest",cost: "23000",location: "Japan",category:"House")
+h4=House.create(title: "Join Home Club",cost: "7800",location: "India",category:"House")
+
+h5=House.create(title: "Apolo 7",cost: "53300",location: "U.S",category:"Apartment")
+h5=House.create(title: "Far From Home",cost: "63700",location: "japan",category:"Apartment")
+h5=House.create(title: "Near Flat",cost: "34004",location: "U.S",category:"Apartment")
+h5=House.create(title: "Bhk Decided",cost: "93006",location: "japan",category:"Apartment")
+h5=House.create(title: "Mountain Apart",cost: "35300",location: "India",category:"Apartment")
