4b75cf7916f8d1df6753b360e867a7e31cef28fc
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-15 19:56:37 +0530

    Add rspec test of association for favourite

--- spec/models/favourite_spec.rb
+++ spec/models/favourite_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Favourite, type: :model do
     favourite = Favourite.new(house_id: 1)
     expect(favourite.save).to be(false)
   end
+  it 'belongs to house association' do
+    favourite = Favourite.reflect_on_association(:house)
+    expect(favourite.macro).to eq(:belongs_to)
+  end
+  it 'belongs to user association' do
+    favourite = Favourite.reflect_on_association(:user)
+    expect(favourite.macro).to eq(:belongs_to)
+  end
 end
