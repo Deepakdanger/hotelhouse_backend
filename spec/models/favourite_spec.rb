549f40c9da82f3b6a7bfe9669d3cf1e04a35b116
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-15 19:56:00 +0530

    Add rspec test for favourite

--- spec/models/favourite_spec.rb
+++ spec/models/favourite_spec.rb
@@ -1,1 +1,12 @@
+require 'rails_helper'
 
+RSpec.describe Favourite, type: :model do
+  it 'is not valid without an house_id' do
+    favourite = Favourite.new(user_id: 1)
+    expect(favourite.save).to be(false)
+  end
+  it 'is not valid without an password' do
+    favourite = Favourite.new(house_id: 1)
+    expect(favourite.save).to be(false)
+  end
+end
