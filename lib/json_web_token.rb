db9ff902f404e1a66027315e18e16530c8020077
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-17 17:53:22 +0530

    Add credential.read in jwt token

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 1200.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.read)
     end
  
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, Rails.application.credentials.read)[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
