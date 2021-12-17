569acf82b9210bcc3d3127c6295c0fa32bb3931e
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-17 15:02:09 +0530

    Add tests errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::API
  
    def authenticate_request
      @current_user = AuthorizeApiRequest.call(request.headers).result
-     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+     render json: { error: @current_user }
    end
 end
