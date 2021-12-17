40a87419ed7f03e2118966f41dbb18e8e6bad758
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-17 15:22:30 +0530

    Add errors in authentication

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -16,6 +16,7 @@ class AuthorizeApiRequest
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
     @user || errors.add(:token, 'Invalid token')
+    @user || errors
   end
 
   def decoded_auth_token
