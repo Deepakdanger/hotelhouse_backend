f05d206db6cfb9080b66518caae24a2601a15201
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-22 21:00:10 +0530

    Add uniqueness in rails database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_141614) do
+ActiveRecord::Schema.define(version: 2021_12_22_152716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_12_03_141614) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
